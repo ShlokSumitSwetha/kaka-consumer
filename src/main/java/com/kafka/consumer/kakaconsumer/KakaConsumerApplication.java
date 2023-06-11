a17ef8fdb751b2b8cde75356c354de8d8a97c45c
Sumith Ksheerasagar <ksherasagar@gmail.com>
2023-06-11 16:27:09 +0100

    Initial commit

---
+++ src/main/java/com/kafka/consumer/kakaconsumer/KakaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.consumer.kakaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kafka/consumer/kakaconsumer/KakaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.consumer.kakaconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
